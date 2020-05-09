a51f2c89b4585ea4d211c0bee24f1925081c0436
Jiazi Cai <caijiazi777@gmail.com>
2020-05-09 17:11:30 -0400

    remove files

--- src/@lekoarts/gatsby-theme-cara/gatsby-plugin-theme-ui/components.js
+++ src/@lekoarts/gatsby-theme-cara/gatsby-plugin-theme-ui/components.js
@@ -1,15 +1,8 @@
 import React from "react"
-import ProjectCard from "../components/project-card"
 import TaskCard from "../components/task-card"
 
 const components = {
   // eslint-disable-next-line react/display-name
-  ProjectCard: ({ link, title, bg, children, img }) => (
-    <ProjectCard link={link} title={title} bg={bg}>
-      {children}
-      <img src={img} alt=""/>
-    </ProjectCard>
-  ),
   TaskCard: ({ link, title, bg, children }) => (
     <TaskCard link={link} title={title} bg={bg}>
       {title}
