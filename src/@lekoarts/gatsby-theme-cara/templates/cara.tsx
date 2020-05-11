9f804bea96ab93a1cc7efa999e6626c1e6c4b54d
Jiazi Cai <caijiazi777@gmail.com>
2020-05-10 22:58:18 -0400

    Update cara.tsx

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -9,15 +9,21 @@ import Tasks from "../components/tasks"
 import {Animated} from "react-animated-css";
  
 
-const Cara = () => (
+const Cara = () => {
+  const isMobile = window.innerWidth >1024;
+  const showAbout = isMobile ? 3 : 4;
+  const showContact = isMobile ? 4 : 5.5;
+  const showPage = isMobile ?  5.04: 6.5;
+  return (
   <Layout>
-    <Parallax pages={6.2}>
+    <Parallax pages={showPage}>
       <Hero offset={0} factor={1} />
       <Tasks offset={1} factor={2} />
-      <About offset={4} factor={1} />
-      <Contact offset={5.2} factor={0} />
+      <About offset={showAbout} factor={1} />
+      <Contact offset={showContact} factor={0} />
     </Parallax>
   </Layout>
 )
+  }
 
 export default Cara
