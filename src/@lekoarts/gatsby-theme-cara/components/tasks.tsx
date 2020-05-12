50a04fa690b990342447ad04b0a89e8f236babc5
Jiazi Cai <caijiazi777@gmail.com>
2020-05-12 16:24:47 -0400

    delete code

--- src/@lekoarts/gatsby-theme-cara/components/tasks.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/tasks.tsx
@@ -8,22 +8,20 @@ import { UpDown, UpDownWide } from "../styles/animations"
 // @ts-ignore
 import Alive from "../../../../public/images/al.png"
 import TaskCard from "./task-card";
-import TaskMDX from "../sections/tasks";
+import TaskMDX from "../sections/task.mdx";
 import { Children } from "react"
 import React, {useState, useEffect, useRef} from 'react'
 import "../../../styles/global.css"
 import SingleTask from "./single-task"
 
 const Tasks = ({ offset, factor = 2 }: { offset: number; factor?: number }) => {
-    const [flip, setFlip] = useState(false);
-    const [height, setHeight] = useState('initial');
+    const [card, setCard] = useState('All');
 
-    const frontEl = useRef();
-    const backEl = useRef();
     const dataPlaylists = [
         {
             id: 101,
             category_id: 1,
+            tech: ['All','React', 'redux', 'MongoDB', 'Node', 'GraphQL'],
             name: 'ALIVE',
             image: "https://res.cloudinary.com/firebeat/image/upload/v1589225659/newAlive_bbl5sv.jpg",
             info:"Developing E-learning platform for New York University with MERN stack",
@@ -35,6 +33,7 @@ const Tasks = ({ offset, factor = 2 }: { offset: number; factor?: number }) => {
             id: 102,
             category_id: 1,
             name: 'Fruit Chat',
+            tech: ['All','React', 'Redux', 'Node', 'Express', 'UI/UX', 'Firebase', 'REST API'],
             image: 'https://res.cloudinary.com/firebeat/image/upload/v1589225658/fc_o3qjah.jpg',
             info: "MERN stack project for chat web application",
             answer: "React, Redux, NodeJS, Express, Material UI, Firebase, REST API",
@@ -45,6 +44,7 @@ const Tasks = ({ offset, factor = 2 }: { offset: number; factor?: number }) => {
             id: 103,
             category_id: 2,
             name: 'To Do List',
+            tech: ['All','React', 'Firebase', 'SCSS'],
             image: "https://res.cloudinary.com/firebeat/image/upload/v1589225659/tdl_wdpfav.jpg",
             info: "Add/delete task, categorize task, light/dark mode, accessibility, quick add task",
             answer: "React, Firebase, React Testing Library, SCSS",
@@ -55,6 +55,7 @@ const Tasks = ({ offset, factor = 2 }: { offset: number; factor?: number }) => {
             id: 104,
             category_id: 1,
             name: 'Pixaland',
+            tech: ['All','React', 'Node', 'MongoDB', 'Heroku'],
             image: "https://res.cloudinary.com/firebeat/image/upload/v1589225659/pl_ty5o0w.jpg",
             info: "Upload photo, search photo, add photographer, search photographer",
             answer: "Node, MongoDB, EJS, Heroku",
@@ -65,6 +66,7 @@ const Tasks = ({ offset, factor = 2 }: { offset: number; factor?: number }) => {
             id: 105,
             category_id: 2,
             name: 'Shop Ramen',
+            tech: ['All','React', 'Netlify', 'UI/UX'],
             image: "https://res.cloudinary.com/firebeat/image/upload/v1589225659/sr_xzuzm9.jpg",
             info: "Dynamic and responsive shopping website",
             answer: "React, Netlify, Bootstrap, Styled-Component",
@@ -75,6 +77,7 @@ const Tasks = ({ offset, factor = 2 }: { offset: number; factor?: number }) => {
             id: 106,
             category_id: 3,
             name: 'COVID Tracker',
+            tech: ['All','REST API', 'React', 'Data Vis'],
             image: "https://res.cloudinary.com/firebeat/image/upload/v1589225658/newCovid-19_qmqf3c.jpg",
             info: "Used REST API to fetch data and Chart.js to display COVID-19 data",
             answer: "React, REST API, CSS Modules, Chart.js",
@@ -85,6 +88,7 @@ const Tasks = ({ offset, factor = 2 }: { offset: number; factor?: number }) => {
             id: 107,
             category_id: 3,
             name: 'Quiz Generator',
+            tech: ['All', 'REST API', 'React', 'UI/UX'],
             image: "https://res.cloudinary.com/firebeat/image/upload/v1589225659/qg_m4rx6w.jpg",
             info: "Used REST API to generate quiz by category and number",
             answer: "React, REST API, CSS Grid",
@@ -95,6 +99,7 @@ const Tasks = ({ offset, factor = 2 }: { offset: number; factor?: number }) => {
             id: 108,
             category_id: 1,
             name: 'Spotify Redesign',
+            tech: ['All', 'React', 'Redux', 'UI/UX'],
             image: "https://res.cloudinary.com/firebeat/image/upload/v1589225659/sd_gdblly.jpg",
             info: "Cloned Spotify's design",
             answer: "React, Redux, CSS, SASS",
@@ -105,6 +110,7 @@ const Tasks = ({ offset, factor = 2 }: { offset: number; factor?: number }) => {
             id: 109,
             category_id: 1,
             name: 'Miniflix',
+            tech: ['All', 'React', 'UI/UX'],
             image: "https://res.cloudinary.com/firebeat/image/upload/v1589225659/newMiniflix_lnt15r.jpg",
             info: "Isomorphic and responsive design web pages of Netflix",
             answer: "React, CSS Grid, Styled-Component",
@@ -112,6 +118,33 @@ const Tasks = ({ offset, factor = 2 }: { offset: number; factor?: number }) => {
             github: "github.com/JiaziCai/Miniflix",
         },
     ]
+
+    const cardOptions = [ 
+      {
+        id:191,
+        name: 'All',
+      },
+      {
+        id: 192,
+        name: 'React'
+      },
+      {
+        id: 193,
+        name: 'Node'
+      },
+      {
+        id: 194,
+        name: 'REST API',
+      },
+      {
+        id: 195,
+        name: 'MongoDB'
+      },
+      {
+        id: 196,
+        name: 'UI/UX'
+      }
+  ]
     return (
   <div>
     <Divider
@@ -130,11 +163,20 @@ const Tasks = ({ offset, factor = 2 }: { offset: number; factor?: number }) => {
             gridGap: [4, 4, 4, 5],
             gridTemplateColumns: [`repeat(auto-fit, minmax(320px, 1fr))`],
             h2: { gridColumn: `-1/1`},
-            justifyContent:`center`
+            justifyContent:`center`,
+            p: {gridColumn: `-1/1`, display: `inline-flex`, margin:`1rem` },
+            button: {color:`heading`},
           }}
         >
-            <TaskMDX />
-            {dataPlaylists.map((playlist, id) => (
+          <TaskMDX />
+            <div>
+                {
+                  cardOptions.map(({id, name}) => (
+                    <p key={id}><button className={card == name ? 'cardButtonActive' : 'cardButton'} onClick={e => setCard(name)}>{name}</button></p>
+                  ))
+                }
+            </div>
+            {dataPlaylists.filter(name => name.tech.includes(card)).map((playlist, id) => (
             <TaskCard bg="linear-gradient(to right, #0d7be3 0%, #4335a4 100%)" playlist={playlist} key={playlist.id} id={playlist.id}>
                 <SingleTask playlist={playlist} key={playlist.id} />
             </TaskCard>
