e257bb3c080eac1326a4ead6432dc8ae42f7c135
Jiazi Cai <caijiazi777@gmail.com>
2020-05-19 14:41:36 -0400

    Update cara.tsx

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -1,34 +1,35 @@
-import React from "react"
-import { Parallax } from "react-spring/renderprops-addons.cjs"
-import Layout from "../components/layout"
-import Hero from "../components/hero"
-import Projects from "../components/projects"
-import About from "../components/about"
-import Contact from "../components/contact"
-import Tasks from "../components/tasks"
+import React from "react";
+import { Parallax } from "react-spring/renderprops-addons.cjs";
+import Layout from "../components/layout";
+import Hero from "../components/hero";
+import Projects from "../components/projects";
+import About from "../components/about";
+import Contact from "../components/contact";
+import Tasks from "../components/tasks";
 
 const Cara = () => {
-  const isMobile = typeof window !== 'undefined' && window.innerWidth >1024;
-  const isBigScreen = typeof window !== 'undefined' && window.innerWidth >= 1568;
+  const isMobile = typeof window !== "undefined" && window.innerWidth > 1024;
+  const isBigScreen =
+    typeof window !== "undefined" && window.innerWidth >= 1568;
   const showTask = isBigScreen ? 1 : 1;
-  const showAbout = isMobile ? (isBigScreen ? 2.3 : 3): 4;
+  const showAbout = isMobile ? (isBigScreen ? 2.3 : 3.2) : 4;
   const heroFactor = isBigScreen ? 1 : 1;
   const taskFactor = isBigScreen ? 1 : 2;
   const aboutFactor = isBigScreen ? 0.5 : 1;
-  const showContact = isMobile ? (isBigScreen ? 2.9 : 4): 5.5;
-  const showPage = isMobile ?  (isBigScreen ? 3.17 : 5.04): 6.5;
+  const showContact = isMobile ? (isBigScreen ? 2.9 : 4) : 5.5;
+  const showPage = isMobile ? (isBigScreen ? 3.17 : 5.04) : 6.5;
   const contactFactor = isBigScreen ? 0.3 : 1;
   return (
-  <Layout>
-    <Parallax pages={showPage}>
-      <a name="head"></a>
-      <Hero offset={0} factor={heroFactor} />
-      <Tasks offset={showTask} factor={taskFactor} />
-      <About offset={showAbout} factor={aboutFactor} />
-      <Contact offset={showContact} factor={contactFactor} />
-    </Parallax>
-  </Layout>
-)
-  }
+    <Layout>
+      <Parallax pages={showPage}>
+        <a name='head'></a>
+        <Hero offset={0} factor={heroFactor} />
+        <Tasks offset={showTask} factor={taskFactor} />
+        <About offset={showAbout} factor={aboutFactor} />
+        <Contact offset={showContact} factor={contactFactor} />
+      </Parallax>
+    </Layout>
+  );
+};
 
-export default Cara
+export default Cara;
