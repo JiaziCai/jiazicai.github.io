a1f12eec56c9257681c89e711c7c723a082a249e
Jiazi Cai <caijiazi777@gmail.com>
2020-06-10 14:45:40 -0400

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c625a9fcf912758842f6.js"
+    "url": "webpack-runtime-d6512260ed809083fde5.js"
   },
   {
     "url": "framework-f7f9b643de0d3b0e5da1.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ee26e3839b74b8e161f78d4bff87b115"
+    "revision": "8de802718d658abaff8a48574416c504"
   },
   {
     "url": "manifest.json",
